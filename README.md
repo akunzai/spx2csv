4ad493c4e6480f9cd9b4ab22c8f506fcee0f3004
Charley Wu <akunzai@gmail.com>
2016-08-30 14:24:49 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# SPX to CSV Converter
+
+## Requirement
+
+- Node.js
+
+## USAGE
+
+```sh
+$ npm install
+$ system_profiler -xml SPSoftwareDataType > ~/Desktop/InstalledApps.spx 
+$ node index.js ~/Desktop/InstalledApps.spx > ~/Desktop/InstalledApps.csv
+```
+
---
+++ index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const plist = require('plist');
+
+if (process.argv.length < 3) {
+    console.log('USAGE: node index.js InstalledApps.spx');
+    return;
+}
+var SPAppsData = plist.parse(fs.readFileSync(process.argv[2], 'utf8'))[0];
+SPAppsData['_items'].forEach(item =>{
+    console.log(`${item['_name']},${item['lastModified']},${item['obtained_from']},${item['path']},${item['version']}`);
+});
