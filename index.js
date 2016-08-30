189c4ebe99041b2eab75092152614dcf6656d7f6
Charley Wu <akunzai@gmail.com>
2016-08-30 15:33:13 +0800

    add header and prevent name contain comma

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ if (process.argv.length < 3) {
     return;
 }
 var SPAppsData = plist.parse(fs.readFileSync(process.argv[2], 'utf8'))[0];
+console.log('name,lastModified,obtained_from,path,version');
 SPAppsData['_items'].forEach(item =>{
-    console.log(`${item['_name']},${item['lastModified']},${item['obtained_from']},${item['path']},${item['version']}`);
+    console.log(`"${item['_name']}","${item['lastModified']}","${item['obtained_from']}","${item['path']}","${item['version']}"`);
 });
