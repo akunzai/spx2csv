4d98313579bffdbf8e96da192eb78be1325e031b
Charley Wu <akunzai@gmail.com>
2020-07-08 11:11:58 +0800

    Listing all SPX fields

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+if (process.argv.length < 3) {
+  console.log('USAGE: node index.js InstalledApps.spx');
+  return;
+}
 const fs = require('fs');
 const plist = require('plist');
-
-if (process.argv.length < 3) {
-    console.log('USAGE: node index.js InstalledApps.spx');
-    return;
-}
-var SPAppsData = plist.parse(fs.readFileSync(process.argv[2], 'utf8'))[0];
-console.log('name,lastModified,obtained_from,path,version');
-SPAppsData['_items'].forEach(item =>{
-    console.log(`"${item['_name']}","${item['lastModified']}","${item['obtained_from']}","${item['path']}","${item['version']}"`);
+const appsData = plist.parse(fs.readFileSync(process.argv[2], 'utf8'))[0];
+let fields = undefined;
+appsData['_items'].forEach((item) => {
+  if (typeof fields === 'undefined') {
+    fields = Object.keys(item).sort();
+    console.log(fields.join(','));
+  }
+  let values = [];
+  fields.forEach((field) => {
+    let value = item[field] || '';
+    values.push(Array.isArray(value) ? `"${value.join('\n')}"` : `"${value}"`);
+  });
+  console.log(values.join(','));
 });
