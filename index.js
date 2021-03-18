a5ebdfb1b519f6460eabf38388a16d2e815993c8
Charley Wu <akunzai@gmail.com>
2021-03-18 08:18:21 +0800

    Check input data format

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ if (process.argv.length < 3) {
   console.log('USAGE: node index.js InstalledApps.spx');
   return;
 }
+
 const fs = require('fs');
 const plist = require('plist');
-const appsData = plist.parse(fs.readFileSync(process.argv[2], 'utf8'))[0];
+const data = plist.parse(fs.readFileSync(process.argv[2], 'utf8'));
 let fields = undefined;
-appsData['_items'].forEach((item) => {
+let items = data[0]['_items'];
+if (typeof items === 'undefined' || !Array.isArray(items)) {
+  throw new Error('Invalid input data format');
+}
+items.forEach((item) => {
   if (typeof fields === 'undefined') {
     fields = Object.keys(item).sort();
     console.log(fields.join(','));
